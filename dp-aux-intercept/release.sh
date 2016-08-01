cfdaf543d154a9b075a94b1c419cd7f01e0e8666
Tim 'mithro' Ansell <mithro@mithis.com>
2016-08-01 15:09:15 +1000

    Adding a script to release the gerbers.

---
+++ dp-aux-intercept/release.sh
@@ -0,0 +1,37 @@
+#!/bin/bash
+
+set -x
+set -e
+
+if [ $(git diff --shortstat 2> /dev/null | tail -n1) != "" ]; then
+	echo "Dirty repo, commit before running."
+	exit 1
+fi
+
+VERSION=$(git describe --long)
+
+echo "Current version: $VERSION"
+
+# Work out the next version
+OUTDIR=gerbers/$VERSION/
+mkdir $OUTDIR
+
+# Update the version embedded in the PCB
+sed -e"s/(gr_text [\$]Id[\$]/(gr_text \"$VERSION\"/" --in-place=.bak dp-aux-intercept.kicad_pcb
+
+# Generate the gerber files
+python ../third_party/gen_gerber_and_drill_files_board.py dp-aux-intercept.kicad_pcb $OUTDIR/
+git add $OUTDIR/*
+
+git commit -m "Creating gerbers $VERSION for dp-aux-intercept"
+
+cd $OUTDIR/
+rm dp-aux-intercept-inner*
+ZIP="dp-aux-intercept-$VERSION.zip"
+zip -r ../$ZIP .
+
+cd ..; md5sum $ZIP
+
+git reset --hard
+
+exit 0
